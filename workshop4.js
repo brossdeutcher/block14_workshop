e337b9322df64ad62eadb9273b44fec93f53f608
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-06-29 12:48:01 -0600

    added question 4

--- workshop4.js
+++ workshop4.js
@@ -3,3 +3,22 @@
 // For each multiple of 5, print "Buzz" instead of the number.
 // For numbers that are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
+// loop to iterate every number between 1 and 100
+for (let i = 1; i <= 100; i++) {
+  // if multiple of 3 and 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    // print "FizzBuzz"
+    console.log(`FizzBuzz`);
+  // if multiple of 3
+  } else if (i % 3 === 0) {
+    // print "Fizz"
+    console.log(`Fizz`);
+  // if multiple of 5
+  } else if (i % 5 === 0) {
+    // print "Buzz"
+    console.log(`Buzz`);
+  } else {
+    // else print number
+    console.log(i);
+  }
+}
